9bdc0e1b55055e7cb2a8e3a6aa4c2f7402e64fe8
MANAR <122207957+ENG9MANAR9SC@users.noreply.github.com>
2025-04-19 13:22:30 +0200

    working on installing vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+
+import hello from './components/hello.vue'
+
+const app = createApp()
+app.component('hello', hello)
+app.mount('#app');
+
+
+
